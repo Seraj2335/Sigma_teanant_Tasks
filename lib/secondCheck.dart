055380ad0869a3c62da67484c8a546fe3e371f5b
Seraj Ahmad <kseraj96@gmail.com>
2022-01-25 22:19:25 +0530

    commit

--- lib/secondCheck.dart
+++ lib/secondCheck.dart
@@ -21,20 +21,35 @@ class _SecondCheckState extends State<SecondCheck> {
   Widget build(BuildContext context) {
     return Container(
       decoration:
-          BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20))),
+          BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(25))),
       width: 350,
       height: 440,
       child: Stack(
         children: [
           Column(
             children: [
+              SizedBox(
+                height: 19,
+              ),
               Container(
-                  width: 200,
-                  height: 200,
-                  margin: EdgeInsets.only(top: 19, right: 77, left: 73),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(15))),
-                  child: Image(image: AssetImage('assets/image 7.png'))),
+                margin: EdgeInsets.only(left: 73, right: 77),
+                child: Image(
+                    width: 200,
+                    height: 140,
+                    image: AssetImage('assets/image 7.png')),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 79, right: 83),
+                child: Image(
+                  width: 102,
+                  height: 58,
+                  image: AssetImage('assets/image 25.png'),
+                ),
+              ),
+              SizedBox(height: 8),
               Container(
                 margin: EdgeInsets.only(
                   left: 30,
@@ -45,40 +60,86 @@ class _SecondCheckState extends State<SecondCheck> {
                   children: [
                     Row(
                       children: [
-                        Text('Product'),
-                        SizedBox(
-                          width: 35,
+                        Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Product',
+                                  style: TextStyle(
+                                      color: Color(0xff595454),
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500)),
+                              Text('Name',
+                                  style: TextStyle(
+                                      color: Color(0xff595454),
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500)),
+                              Text('2345678-678',
+                                  style: TextStyle(
+                                      color: Color(0xff595454),
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          ),
                         ),
-                        Text('Carbon2'),
+                        SizedBox(
+                          width: 62,
+                        ),
+                        Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Carton 2',
+                                  style: TextStyle(
+                                      color: Color(0xff595454),
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600)),
+                              Text('Unit 4',
+                                  style: TextStyle(
+                                      color: Color(0xff595454),
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600)),
+                              Text('10 PCS/CTN',
+                                  style: TextStyle(
+                                      color: Color(0xff595454),
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600)),
+                            ],
+                          ),
+                        )
                       ],
+                    ),
+                    Text('Available: ' + "10 PCS",
+                        style: TextStyle(
+                            color: Color(0xff595454),
+                            fontFamily: 'Montserrat',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500)),
+                    SizedBox(
+                      height: 10,
                     ),
                     Row(
                       children: [
-                        Text('Name'),
+                        Text('Carton Number',
+                            style: TextStyle(
+                                color: Color(0xffFFA000),
+                                fontFamily: 'Montserrat',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600)),
                         SizedBox(
-                          width: 35,
+                          width: 23,
                         ),
-                        Text('Unit 4'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('2345678-678'),
-                        SizedBox(
-                          width: 35,
-                        ),
-                        Text('10 PCS/CTN'),
-                      ],
-                    ),
-                    Text('Available: ' + "10 PCS"),
-                    Row(
-                      children: [
-                        Text('Carton Number'),
                         Container(
                           width: 59,
                           height: 30,
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
                             border:
                                 Border.all(width: 1, color: Color(0xffFFA000)),
                           ),
@@ -94,33 +155,40 @@ class _SecondCheckState extends State<SecondCheck> {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 22,
+              ),
               Container(
                 margin: EdgeInsets.only(left: 101),
                 child: Row(
                   children: [
                     Text(
                       'Second Check',
-                      style: TextStyle(color: Color(0xff1AAC0D)),
+                      style: TextStyle(
+                          color: Color(0xff1AAC0D),
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Montserrat'),
                     ),
-                    SizedBox(
-                      width: 6,
-                    ),
-                    Checkbox(
-                        value: isChecked,
-                        onChanged: (newValue) {
-                          setState(() {
-                            isChecked = newValue!;
-                          });
-                        })
+                    Container(
+                        child: Checkbox(
+                            checkColor: Color(0xff1AAC0D),
+                            activeColor: Colors.white,
+                            side: BorderSide(color: Color(0xff1AAC0D)),
+                            value: isChecked,
+                            onChanged: (newValue) {
+                              setState(() {
+                                isChecked = newValue!;
+                              });
+                            }))
                   ],
                 ),
               ),
             ],
           ),
-          Container(
-              margin: EdgeInsets.only(left: 315, right: 16, top: 11),
-              child: IconButton(
-                  onPressed: null, icon: Image.asset('assets/X.png')))
+          IconButton(
+              padding: EdgeInsets.only(left: 245, top: 11, right: 16),
+              onPressed: () => Navigator.pop(context),
+              icon: Image.asset('assets/X.png'))
         ],
       ),
     );
